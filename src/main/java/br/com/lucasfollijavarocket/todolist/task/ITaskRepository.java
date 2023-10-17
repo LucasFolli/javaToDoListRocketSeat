20bd2735b77a3e9eb17446faac046c7c910f56c9
Lucas Folli <contato_lucasalves@yahoo.com>
2023-10-17 15:04:34 -0300

    Fixing

--- src/main/java/br/com/lucasfollijavarocket/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/lucasfollijavarocket/todolist/task/ITaskRepository.java
@@ -3,11 +3,10 @@ package br.com.lucasfollijavarocket.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
     List<TaskModel> findByIdUser(UUID idUser);
 
--- src/main/java/br/com/lucasfollijavarocket/todolist/user/IUserRepository.java
+++ src/main/java/br/com/lucasfollijavarocket/todolist/user/IUserRepository.java
@@ -3,9 +3,7 @@ package br.com.lucasfollijavarocket.todolist.user;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {
     UserModel findByUsername(String username);
 
